3d29f6e9966cfa2cec357110c4a917836814f23d
LoulouNoLegend <40952934+LoulouNoLegend@users.noreply.github.com>
2026-02-06 20:58:53 -0500

    new nice text with pink :3

--- Features/Overlay.cs
+++ Features/Overlay.cs
@@ -10,6 +10,8 @@ namespace OddFramework.Features
     public class OverlayFeature : InFeature
     {
         private bool _show = true;
+        private GUIStyle titleStyle;
+        private GUIStyle lineStyle;
 
         public void Init() { }
         public void OnSceneLoaded(int buildIndex, string sceneName) { }
@@ -25,10 +27,24 @@ namespace OddFramework.Features
         {
             if (!_show) return;
 
+            if (titleStyle == null)
+            {
+                titleStyle = new GUIStyle();
+                titleStyle.normal.textColor = Color.hotPink;
+                titleStyle.fontSize = 24;
+            }
+
+            if (lineStyle == null)
+            {
+                lineStyle = new GUIStyle();
+                lineStyle.normal.textColor = Color.white;
+                lineStyle.fontSize = 16;
+            }
+
             UI.Panel(new Rect(10, 10, 420, 120), () => {
-                GUI.Label(new Rect(10, 10, 500, 40), "OddFramework " + OddFrameworkMod.Instance.modVersion);
-                GUI.Label(new Rect(10, 52, 400, 20), "DiscordRPC: " + OddFrameworkMod.Instance.discordRpcState);
-                GUI.Label(new Rect(10, 74, 400, 20), "F8: toggle this menu");
+                GUI.Label(new Rect(10, 10, 500, 30), "OddFramework " + OddFrameworkMod.Instance.modVersion, titleStyle);
+                GUI.Label(new Rect(10, 42, 400, 20), "DiscordRPC: " + OddFrameworkMod.Instance.discordRpcState, lineStyle);
+                GUI.Label(new Rect(10, 64, 400, 20), "F8: toggle this menu", lineStyle);
             });
         }
     }
